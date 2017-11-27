e902623a2476d30fb214e4e6f464476fe09255e9
elliot richardson <ear78@aol.com>
2017-11-26 22:53:48 -0600

    added new routes for footer navigation and designed footer

--- app/router.js
+++ app/router.js
@@ -10,6 +10,15 @@ Router.map(function() {
   this.route('index', {path: '/'});
   this.route('products');
   this.route('about');
+  this.route('news');
+  this.route('events');
+  this.route('careers');
+  this.route('contact');
+  this.route('inocc');
+  this.route('myitl');
+  this.route('privacy-policy');
+  this.route('faa-guidelines');
+  this.route('terms-of-service');
 });
 
 export default Router;
